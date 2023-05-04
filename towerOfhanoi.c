dd54fa6392befd3a9d2abbbe8d856420e8d51799
Sushantchaudhary1 <119941898+Sushantchaudhary1@users.noreply.github.com>
2023-05-04 09:48:33 +0530

    Update towerOfhanoi.c

--- towerOfhanoi.c
+++ towerOfhanoi.c
@@ -7,7 +7,7 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
 	{
 		printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
 		return;
-	}
+	}else
 	towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
 	printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
 	towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
